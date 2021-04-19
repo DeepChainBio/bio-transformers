bec1ca34d2aa839916c992f39178104fda2f22d1
aurelien delfosse <a.delfosse@instadeep.com>
2021-04-19 19:40:12 +0200

    add pytest

--- test/test_loglikelihood.py
+++ test/test_loglikelihood.py
@@ -1,6 +1,13 @@
 """Test module for testing loglikelihood function"""
 from biotransformers import BioTransformers
 
+test_sequences = [
+    "MKTVRQERLKSIVRILERSKEPVSGAQLAEELSVSRQVIVQDIAYLRSLGYNIVATPRGYVLAGG",
+    "KALTARQQEVFDLIRDHISQTGMPPTRAEIAQRLGFRSPNAAEEHLKALARKGVIEIVSGASRGIRLLQEE",
+]
+
 
 def test_esm_loglikelihood():
-    pass
+    test_trans = BioTransformers()
+    loglikelihood = test_trans.compute_loglikelihood(test_sequences)
+    assert isinstance(loglikelihood, list)
