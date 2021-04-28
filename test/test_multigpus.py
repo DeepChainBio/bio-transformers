eac43be4bd96eb46cbab9db797d9c374c163823c
a.delfosse <a.delfosse@instadeep.com>
2021-04-28 16:31:09 +0000

    add multigpus test

--- test/test_multigpus.py
+++ test/test_multigpus.py
@@ -5,5 +5,6 @@ data = biodatasets.load_dataset('swissProt')
 X, y = data.to_npy_arrays(input_names=["sequence"])
 X = X[0]
 
+#tr = BioTransformers("esm1b_t33_650M_UR50S",device="cuda",multi_gpu=True)
 tr = BioTransformers("protbert",device="cuda",multi_gpu=True)
 tr.compute_embeddings(X[:2000],batch_size=48)
