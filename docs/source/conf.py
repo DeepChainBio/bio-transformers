680d2d409ebd1d1d523f21df33b61d1770982d75
aurelien delfosse <a.delfosse@instadeep.com>
2021-05-02 12:01:48 +0200

    add rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,5 @@
+# type: ignore
+# flake8: noqa
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -16,13 +18,14 @@
 
 
 # -- Project information -----------------------------------------------------
+import sphinx_rtd_theme
 
-project = 'bio-transformers'
-copyright = '2021, instadeep'
-author = 'instadeep'
+project = "bio-transformers"
+copyright = "2021, instadeep"
+author = "instadeep"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = "0.0.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,11 +33,10 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -44,12 +46,13 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+
+extensions = [
+    "sphinx_rtd_theme",
+]
+
+html_theme = "sphinx_rtd_theme"
