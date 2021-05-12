55e1e771c65747dadeef6283798a59d777474939
aurelien delfosse <delfosseaurelien@gmail.com>
2021-05-12 08:57:51 +0000

    update test_multigpus

--- biotransformers/tests/multigpus.py
+++ biotransformers/tests/multigpus.py
@@ -1,16 +1,15 @@
 # type: ignore
 import biodatasets
-from biotransformers import BioTransformers
 import numpy as np
+from biotransformers import BioTransformers
 
 data = biodatasets.load_dataset("swissProt")
 X, y = data.to_npy_arrays(input_names=["sequence"])
 X = X[0]
 
 # Train on small sequence
-length = np.array(list(map(len, X))) < 500
+length = np.array(list(map(len, X))) < 200
+print("Number of proteins {}".format(sum(length)))
 
-# tr = BioTransformers("esm1b_t33_650M_UR50S",device="cuda",multi_gpu=True)
-tr = BioTransformers("esm1_t12_85M_UR50S", device="cuda", multi_gpu=True)
-# tr.compute_embeddings(X[:2000], batch_size=48)  # noqa
-tr.train_masked(X[length], batch_size=8, epochs=20)
+tr = BioTransformers("esm1_t6_43M_UR50S", device="cuda", multi_gpu=True)
+tr.train_masked(X[length], batch_size=16, epochs=20)
