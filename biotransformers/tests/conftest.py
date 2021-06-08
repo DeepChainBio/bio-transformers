740049238c78a12282b9b5b49e53d94d710773bc
aurelien delfosse <a.delfosse@instadeep.com>
2021-06-08 17:12:33 +0200

    (test) confest to improve memory management

--- biotransformers/tests/conftest.py
+++ biotransformers/tests/conftest.py
@@ -2,11 +2,11 @@ import pytest
 from biotransformers import BioTransformers
 
 test_models = [
-    "esm1_t34_670M_UR100",
+    # "esm1_t34_670M_UR100",
     "esm1_t6_43M_UR50S",
     "esm1b_t33_650M_UR50S",
     "protbert",
-    "protbert_bfd",
+    # "protbert_bfd",
 ]
 
 
