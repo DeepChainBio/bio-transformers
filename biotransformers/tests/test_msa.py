d698c41084763760793903c8bfe4e30400d47a5a
aurelien delfosse <a.delfosse@instadeep.com>
2021-07-01 10:28:56 +0200

    test: update msa_test

--- biotransformers/tests/test_msa.py
+++ biotransformers/tests/test_msa.py
@@ -3,7 +3,7 @@ from glob import glob
 import numpy as np
 from biotransformers import BioTransformers
 
-path_msa = "../../data/msa"
+path_msa = "data/msa"
 model = BioTransformers("esm_msa1_t12_100M_UR50S")
 
 
