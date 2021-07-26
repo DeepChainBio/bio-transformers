4af0328ac898fed8bc82757117931e8886193350
a.delfosse <a.delfosse@instadeep.com>
2021-07-26 08:54:38 +0200

    docs: update release version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = "2021, InstaDeep"
 author = "InstaDeep"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.1.7"
 
 
 # -- General configuration ---------------------------------------------------
