b28eb95ad2a8d38109ecfd06c16fad95b4f26685
thomas <t.pierrot@instadeep.com>
2021-09-16 11:35:01 +0200

    added support for distributed batch sampling

--- biotransformers/lightning_utils/data.py
+++ biotransformers/lightning_utils/data.py
@@ -1,10 +1,12 @@
 import functools
 import random
+import math
 from collections import OrderedDict
-from typing import Callable, List, Sequence, Tuple
+from typing import Callable, List, Sequence, Tuple, Optional
 
 import numpy as np
 import torch
+import torch.distributed as dist
 from esm.data import BatchConverter
 from torch.utils.data import DataLoader, Dataset, Sampler
 
@@ -187,6 +189,7 @@ def get_batch_indices(
     sequence_strs: List[str],
     toks_per_batch: int,
     crop_sizes: Tuple[int, int] = (600, 1200),
+    seed: int = 0,
 ) -> List[List[List[Tuple[int, int]]]]:
     """
     This sampler aims to create batches that do not contain fixed number of sequences
@@ -211,6 +214,7 @@ def get_batch_indices(
         toks_per_batch (int): Maximum number of token per batch
         extra_toks_per_seq (int, optional): . Defaults to 0.
         crop_sizes (Tuple[int, int]): min and max sequence lengths when cropping
+        seed (int): seed to be used for random generator
 
     Returns:
         List: List of batches indexes and lengths
@@ -218,14 +222,18 @@ def get_batch_indices(
     min_size, max_size = crop_sizes
     buffer_type = List[Tuple[int, int]]
 
-    def crop_length(length: int) -> int:
-        crop_size = random.randint(min_size, max_size) - 2
+    rand_generator = random.Random(seed)
+
+    def crop_length(length: int, random_generator: random.Random) -> int:
+        crop_size = random_generator.randint(min_size, max_size) - 2
         if length > crop_size:
             return crop_size
         else:
             return length
 
-    sizes = [(crop_length(len(s)), i) for i, s in enumerate(sequence_strs)]
+    sizes = [
+        (crop_length(len(s), rand_generator), i) for i, s in enumerate(sequence_strs)
+    ]
     min_length, max_length = min([t[0] for t in sizes]), max([t[0] for t in sizes])
 
     # if there is a large gap between min and max size, sort the list
@@ -233,7 +241,7 @@ def get_batch_indices(
         sizes.sort()
     # otherwise shuffle it
     else:
-        random.shuffle(sizes)
+        rand_generator.shuffle(sizes)
 
     batches: List[List[buffer_type]] = []
     buffer: buffer_type = []
@@ -258,7 +266,7 @@ def get_batch_indices(
 
     _flush_current_buf()
 
-    random.shuffle(batches)
+    rand_generator.shuffle(batches)
     return batches
 
 
@@ -296,6 +304,98 @@ class BatchWithConstantNumberTokensSampler(Sampler):
             toks_per_batch=self._toks_per_batch,
             crop_sizes=self._crop_sizes,
         )
+
+
+class DistributedBatchWithConstantNumberTokensSampler(Sampler):
+    """
+    Sampler that returns batches of sequences indices in the dataset so that to ensure
+    not a fixed number of sequences per batch but rather a fixed number of tokens per
+    batch. This sampler also takes into account that we may want to crop dynamically
+    sequences when sampling and thus returns in addition to indices, desired cropping
+    lengths to inform the dataloader. This version of the sampler is distributed to
+    be used with DDP accelerator.
+    """
+
+    def __init__(
+        self,
+        sequence_strs: List[str],
+        toks_per_batch: int,
+        crop_sizes: Tuple[int, int] = (512, 1024),
+        num_replicas: Optional[int] = None,
+        rank: Optional[int] = None,
+        seed: int = 0,
+    ):
+        Sampler.__init__(self, data_source=None)
+
+        # Replicate Torch Distributed Sampler logic
+        if num_replicas is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            num_replicas = dist.get_world_size()
+        if rank is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            rank = dist.get_rank()
+        if rank >= num_replicas or rank < 0:
+            raise ValueError(
+                "Invalid rank {}, rank should be in the interval"
+                " [0, {}]".format(rank, num_replicas - 1)
+            )
+
+        self._num_replicas = num_replicas
+        self._rank = rank
+        self._epoch = 0
+        self._seed = seed
+
+        self._sequence_strs = sequence_strs
+        self._toks_per_batch = toks_per_batch
+        self._crop_sizes = crop_sizes
+        self._init_batches = get_batch_indices(
+            sequence_strs=sequence_strs,
+            toks_per_batch=toks_per_batch,
+            crop_sizes=crop_sizes,
+            seed=self._seed + self._epoch,
+        )
+        self._num_samples = math.ceil(len(self._init_batches) / self._num_replicas)
+        self._total_size = self._num_samples * self._num_replicas
+
+    def __len__(self) -> int:
+        return self._num_samples
+
+    def set_epoch(self, epoch: int) -> None:
+        self._epoch = epoch
+
+    def __iter__(self):
+
+        # generate batches with constant number of tokens
+        batches = get_batch_indices(
+            sequence_strs=self._sequence_strs,
+            toks_per_batch=self._toks_per_batch,
+            crop_sizes=self._crop_sizes,
+            seed=self._seed + self._epoch,
+        )
+
+        # shuffle the indices
+        rng = np.random.default_rng(seed=self._seed + self._epoch)
+        indices = list(rng.permutation(len(batches)))
+
+        # add extra samples to make it evenly divisible
+        padding_size = self._total_size - len(indices)
+        if padding_size <= len(indices):
+            indices += indices[:padding_size]
+        else:
+            indices += (indices * math.ceil(padding_size / len(indices)))[:padding_size]
+        assert len(indices) == self._total_size
+
+        # subsample (to get batches for this worker)
+        indices = indices[self._rank : self._total_size : self._num_replicas]
+        assert len(indices) == self._num_samples
+
+        # get corresponding batches
+        batches = [batches[i] for i in indices]
+
+        # return iterator
+        yield from batches
 
 
 class BatchWithConstantNumberTokensDataset(Dataset):
@@ -348,7 +448,7 @@ def create_dataloader(
     """
 
     dataset = BatchWithConstantNumberTokensDataset(sequences)
-    batch_sampler = BatchWithConstantNumberTokensSampler(
+    batch_sampler = DistributedBatchWithConstantNumberTokensSampler(
         sequence_strs=sequences, toks_per_batch=toks_per_batch, crop_sizes=crop_sizes
     )
 
